81125bc96db29fd130670df1dd243357300c9733
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-10-29 08:45:18 +0530

    Time: 4 ms (34.11%), Space: 6.2 MB (79.92%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.cpp
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -2,27 +2,22 @@ class Solution {
 public:
     bool isValid(string s) {
         stack<char> myStack;
+        myStack.push('#');
         for(auto x:s){
             if(x=='(' || x=='[' || x=='{'){
                 myStack.push(x);
             }
             else{
-                if(!myStack.empty()){
-                    char top = myStack.top();
-                    if(top=='(' && x==')' || top=='[' && x==']' || top=='{' && x=='}'){
-                        myStack.pop();
-                    }
-                    else{
-                        return false;
-                        // break;
-                    }
+                char top = myStack.top();
+                if(top=='(' && x==')' || top=='[' && x==']' || top=='{' && x=='}'){
+                    myStack.pop();
                 }
                 else{
                     return false;
                 }
             }
         }
-        if(myStack.empty()){
+        if(myStack.top()=='#'){
             return true;   
         }
         return false;

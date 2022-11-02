41afa6bbfc1ebc4ee9a4c3f23ce8b16b2a8c88a0
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-11-02 13:05:50 +0530

    Time: 4 ms (29.93%), Space: 6.6 MB (42.79%) - LeetHub

---
+++ 0433-minimum-genetic-mutation/0433-minimum-genetic-mutation.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minMutation(string start, string end, vector<string>& bank) {
+        unordered_set <string> myBank(bank.begin(),bank.end());
+        unordered_set<string> visited;
+        queue<pair<string,int>> q;
+        q.push({start,0});
+        visited.insert(start);
+        vector<char> c = {'A','C','G','T'};
+        while(!q.empty()){
+            string s = q.front().first;
+            int level = q.front().second;
+            if(s==end){
+                return level;
+            }
+            q.pop();
+            string new_gene ;
+            for(int i = 0 ; i<8 ; i++){
+                for (auto x : c){
+                    // cout<<"hi"<<endl;
+                    new_gene = s.substr(0,i) + x + s.substr(i+1);
+                    // cout<<new_gene<<endl;
+                    // cout<<end<<endl;
+                    // cout<<"--------"<<endl;
+                    if(myBank.find(new_gene)!=myBank.end() && visited.find(new_gene)==visited.end()){
+                        q.push({new_gene,level+1});
+                        visited.insert(new_gene);
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+};

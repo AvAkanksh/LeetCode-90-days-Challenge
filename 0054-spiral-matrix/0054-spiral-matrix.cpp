149778ab74354b541a5ece4fc277a102cffefc72
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-11-07 00:37:25 +0530

    Time: 0 ms (100.00%), Space: 6.9 MB (72.11%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.cpp
+++ 0054-spiral-matrix/0054-spiral-matrix.cpp
@@ -4,32 +4,32 @@ public:
         int rows = matrix.size();
         int cols = matrix[0].size();
         vector<int> ans;
-            int t = 0 ;
-            int b = rows-1;
-            int l = 0 ;
-            int r = cols-1;
-            while(t<=b && l<=r){
-                for(int i = l ; i<=r ; i++){
-                    ans.push_back(matrix[t][i]);
-                }
-                t++;
-                for(int j = t ; j<=b ; j++){
-                    ans.push_back(matrix[j][r]);
-                }
-                r--;
-                if(ans.size()==rows*cols){
-                    break;
-                }
-                for(int i = r ; i>=l ; i--){
-                    ans.push_back(matrix[b][i]);
-                }
-                b--;
-                for(int j = b ; j>=t ; j--){
-                    ans.push_back(matrix[j][l]);
-                }
-                l++;
-            }       
-            return ans;
+        int t = 0 ;
+        int b = rows-1;
+        int l = 0 ;
+        int r = cols-1;
+        while(t<=b && l<=r){
+            for(int i = l ; i<=r ; i++){
+                ans.push_back(matrix[t][i]);
+            }
+            t++;
+            for(int j = t ; j<=b ; j++){
+                ans.push_back(matrix[j][r]);
+            }
+            r--;
+            if(ans.size()==rows*cols){
+                break;
+            }
+            for(int i = r ; i>=l ; i--){
+                ans.push_back(matrix[b][i]);
+            }
+            b--;
+            for(int j = b ; j>=t ; j--){
+                ans.push_back(matrix[j][l]);
+            }
+            l++;
+        }       
+        return ans;
         
     }
 };

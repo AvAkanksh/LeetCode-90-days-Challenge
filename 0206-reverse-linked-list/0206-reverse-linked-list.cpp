e18ecb72dffa345eb082fc9743a5a7de1c7e797d
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-11-10 22:53:36 +0530

    Time: 13 ms (48.62%), Space: 8.4 MB (40.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -10,25 +10,27 @@
  */
 class Solution {
 public:
-//     ListNode* reverseList(ListNode* head) {
-//         ListNode* prev = NULL;
-//         ListNode* curr = head;
-//         while(curr){
-//             ListNode* next = curr->next;
-//             curr->next = prev;
-//             prev = curr;
-//             curr = next;
-//         }
-//         return prev;
-//     }
-    
+//     Interative Solution
     ListNode* reverseList(ListNode* head) {
-        if(head==NULL || head->next == NULL){
-            return head;
+        ListNode* prev = NULL;
+        ListNode* curr = head;
+        while(curr){
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
         }
-        ListNode* newHead = reverseList(head->next);
-        head->next->next = head;
-        head->next = NULL;
-        return newHead;
+        return prev;
     }
+    
+//     Recursive Solution
+    // ListNode* reverseList(ListNode* head) {
+    //     if(head==NULL || head->next == NULL){
+    //         return head;
+    //     }
+    //     ListNode* newHead = reverseList(head->next);
+    //     head->next->next = head;
+    //     head->next = NULL;
+    //     return newHead;
+    // }
 };

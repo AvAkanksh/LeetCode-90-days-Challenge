1fa14c714155633816a730558270fb6863ee6f8f
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-12-08 17:53:17 +0530

    Time: 0 ms (100.00%), Space: 6 MB (40.11%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.cpp
@@ -1,11 +1,14 @@
 class Solution {
-public:
+public:    
     int numTrees(int n) {
-        if(n<=1){return 1;}
-        int sum = 0;
-        for(int i = 1 ; i<=n ; i++){
-            sum+=numTrees(i-1)*numTrees(n-i);
+        vector<int> dp(n+1,0);
+        dp[0]=1;
+        dp[1]=1;
+        for(int i = 2; i<=n ; i++){
+            for(int j = 1 ; j <=i ;j++){
+                dp[i] += dp[j-1]*dp[i-j];
+            }
         }
-        return sum;
+        return dp[n];
     }
 };

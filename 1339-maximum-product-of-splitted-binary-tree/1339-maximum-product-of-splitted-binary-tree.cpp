33cdbc58f58546f7b032300298ebac9d6897fd77
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-12-10 20:38:52 +0000

    Time: 318 ms (33.83%), Space: 90.5 MB (24.91%) - LeetHub

--- 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
+++ 1339-maximum-product-of-splitted-binary-tree/1339-maximum-product-of-splitted-binary-tree.cpp
@@ -13,25 +13,48 @@ static int MOD=1e9+7;
 class Solution {
 public:
     long long totalTreeSum=0,result=0;
-    void getTotalTreeSum(TreeNode* root)    //Get total sum of the tree.
-    {
-        if(!root)
+    // void getTotalTreeSum(TreeNode* root)    //Get total sum of the tree.
+    // {
+    //     if(!root)
+    //         return;
+    //     totalTreeSum+=root->val;
+    //     getTotalTreeSum(root->left);
+    //     getTotalTreeSum(root->right);
+    // }
+    // int SumUnder(TreeNode* root)             //Get the totalSum under the node `root` including root.
+    // {
+    //    if(!root)
+    //         return 0;
+    //    int sumUnderLeft=SumUnder(root->left),sumUnderRight=SumUnder(root->right); //Get the sum of left and right subtree under node 'root'
+    //    result=max({result,(totalTreeSum-sumUnderLeft)*sumUnderLeft,(totalTreeSum-sumUnderRight)*sumUnderRight});    //Get the max product after making left or right subtrees as seprarate tree.
+    //    return sumUnderLeft+sumUnderRight+root->val;
+    // }
+    
+    void getTotalSum(TreeNode* root){
+        if(root==NULL){
             return;
-        totalTreeSum+=root->val;
-        getTotalTreeSum(root->left);
-        getTotalTreeSum(root->right);
+        }
+        totalTreeSum += root->val;
+        getTotalSum(root->left);
+        getTotalSum(root->right);
+        
     }
-    int SumUnder(TreeNode* root)             //Get the totalSum under the node `root` including root.
-    {
-       if(!root)
+    
+    int SumUnder(TreeNode* root){
+        if(!root){
             return 0;
-       int sumUnderLeft=SumUnder(root->left),sumUnderRight=SumUnder(root->right); //Get the sum of left and right subtree under node 'root'
-       result=max({result,(totalTreeSum-sumUnderLeft)*sumUnderLeft,(totalTreeSum-sumUnderRight)*sumUnderRight});    //Get the max product after making left or right subtrees as seprarate tree.
-       return sumUnderLeft+sumUnderRight+root->val;
+        }
+        int sumUnderLeft = SumUnder(root->left);
+        int sumUnderRight = SumUnder(root->right);
+        result = max({result,(totalTreeSum-sumUnderLeft)*sumUnderLeft,(totalTreeSum-sumUnderRight)*sumUnderRight});
+        return sumUnderLeft + sumUnderRight + root->val;
+        
     }
+    
+    
     int maxProduct(TreeNode* root) 
     {
-        getTotalTreeSum(root);
+        getTotalSum(root);
         SumUnder(root);
         return result%MOD;
     }

761418c21fb5d5691388712457e6621e1ea36b4a
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2022-12-29 20:17:05 +0530

    Time: 699 ms (80.69%), Space: 129.6 MB (51.59%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    typedef pair<int,int> T;
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        int N = tasks.size();
+        for(int i = 0 ; i<N ; i++){
+            tasks[i].push_back(i);
+        }
+        priority_queue<T , vector<T> , greater<>> pq;
+        int i = 0;
+        vector<int> ans;
+        sort(tasks.begin(), tasks.end());
+        long time = 0;
+        while(i<N || pq.size()){
+            if(pq.empty()){
+                time= max(time,(long) tasks[i][0]);
+            }
+            while(i<N && time>=tasks[i][0]){
+                pq.emplace(tasks[i][1],tasks[i][2]);
+                i++;
+            }
+            auto [pro,index] = pq.top();
+            pq.pop();
+            time+=pro;
+            ans.push_back(index);
+        }
+        return ans;
+    }
+};

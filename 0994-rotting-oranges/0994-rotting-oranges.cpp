18cc47c128538ba1e13a5945a604e2ccfbc63baf
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2023-01-04 01:28:56 +0530

    Time: 39 ms (5.03%), Space: 13.2 MB (39.49%) - LeetHub

---
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int fresh_count = 0;
+        queue<pair<pair<int,int>,int>> q;
+        for(int i = 0 ; i<n ; i++){
+            for(int j = 0  ; j<m ; j++){
+                if(grid[i][j]==2){
+                    q.push({{i,j},0});
+                }
+            }
+        }
+        int tmax = 0;
+
+        vector<int> d = {0,-1,0,1,0};
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            tmax = max(tmax,t);
+            q.pop();
+            cout<<r<<" "<<c<<endl;
+            for(int i = 0 ; i< 4 ; i++){
+                int nx = r+d[i];
+                int ny = c+d[i+1];
+                if(nx>=0 && ny>=0 && nx<n &&ny <m && grid[nx][ny]==1){
+                    cout<<"\t"<<nx<<" "<<ny<<endl;
+                    grid[nx][ny] = 2;
+                    q.push({{nx,ny},t+1});
+                }
+            }
+        }
+        for(auto x: grid){
+            for(auto y : x){
+                if(y==1){
+                    return -1;
+                }
+            }
+        }
+        return tmax;
+    }
+};

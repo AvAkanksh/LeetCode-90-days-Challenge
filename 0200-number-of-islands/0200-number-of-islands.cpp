539d74388bd5d252b97eb9cb5e6d553b4f72e809
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2023-01-04 00:45:00 +0530

    Time: 97 ms (32.17%), Space: 12.3 MB (86.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
-    void dfs(int i , int j , vector<vector<char>> &grid){
-        grid[i][j]='2';
-        vector<int>d = {0,-1,0,1,0};
+    void dfs(int i , int j , vector<vector<char>> &grid,vector<int> &d){
+        if(i<0 || j<0 || i==grid.size() || j== grid[0].size() || grid[i][j]=='0'){return;}
+        grid[i][j]='0';
         for(int x = 0 ; x<4 ; x++){
-            int nx = i+d[x];
-            int ny = j+d[x+1];
-            
-            if(nx>=0 && nx<grid.size() && ny>=0 &&ny<grid[0].size() && grid[nx][ny]=='1'){
-                dfs(nx,ny,grid);
-            }
+            dfs(i+d[x],j+d[x+1],grid,d);
         }
     }
     
     int numIslands(vector<vector<char>>& grid) {
         int ans = 0;
+        vector<int>d = {0,-1,0,1,0};
         for(int i = 0 ; i<grid.size() ; i++){
             for(int j = 0 ; j<grid[0].size() ;j++){
                 if(grid[i][j]=='1'){
                     ans++;
-                    dfs(i,j,grid);
+                    dfs(i,j,grid,d);
                 }
             }
         }

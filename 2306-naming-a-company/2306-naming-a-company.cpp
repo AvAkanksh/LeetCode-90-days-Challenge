3836ac0160de1e8da5aa5379c17064d52ec0336d
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2023-02-09 15:53:23 +0530

    Time: 696 ms (67.48%), Space: 83.6 MB (35.77%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -1,23 +1,23 @@
-class Solution
-{
-    public:
-        long long distinctNames(vector<string> &ideas) {
-            long long ans = 0;
-
-            // Group strings by initials
-            unordered_set<string> suffixes[26];
-
-            for (const string &idea : ideas)
-                suffixes[idea[0] - 'a'].insert(idea.substr(1));
-
-            for (int i = 0; i < 25; ++i)
-                for (int j = i + 1; j < 26; ++j) {
-                    int count = 0;
-                    for (const string &suffix : suffixes[i])
-                        if (suffixes[j].count(suffix))
-                            ++count;
-                    ans += 2 * (suffixes[i].size() - count) * (suffixes[j].size() - count);
-                }
-            return ans;
+class Solution {
+public:
+    long long distinctNames(vector<string>& ideas) {
+        vector<unordered_set<string>>suf(26);
+        long long res=0;
+        for(auto & idea :ideas){
+            suf[idea[0]-'a'].insert(idea.substr(1));
         }
+        for(int i=0;i<suf.size();i++){
+            for(int j=0;j<suf.size();j++){
+                if(i==j)continue;
+                int same=0;
+                for(auto & s :suf[i]){
+                    if(suf[j].count(s))same++;
+                }
+                int dist1=suf[i].size()-same;
+                int dist2=suf[j].size()-same;
+                res+=dist1*dist2;
+            }
+        }
+        return res;
+    }
 };

f3cb4122c24ed6c7c874f12e83bc7d9334798130
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2023-02-09 16:07:25 +0530

    Time: 660 ms (73.17%), Space: 83.6 MB (35.77%) - LeetHub

--- 2306-naming-a-company/2306-naming-a-company.cpp
+++ 2306-naming-a-company/2306-naming-a-company.cpp
@@ -2,15 +2,15 @@ class Solution {
 public:
     long long distinctNames(vector<string>& ideas) {
         unordered_map<char,unordered_set<string>> hash;
-        for(auto str : ideas){
+        for(auto &str : ideas){
             hash[str[0]].insert(str.substr(1));
         }
         long long ans = 0;
-        for(auto x : hash){
-            for(auto y : hash){
+        for(auto &x : hash){
+            for(auto &y : hash){
                 if(x.first==y.first){continue;}
                 int intersection = 0;
-                for(auto word : x.second){
+                for(auto &word : x.second){
                     if(y.second.count(word)){
                         intersection++;
                     }

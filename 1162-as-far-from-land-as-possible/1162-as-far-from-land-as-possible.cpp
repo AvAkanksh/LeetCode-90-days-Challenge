260fd9ed34a2231893bf284a02f67e4e7be50fd1
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2023-02-10 15:58:50 +0530

    Time: 444 ms (7.65%), Space: 21.5 MB (43.00%) - LeetHub

---
+++ 1162-as-far-from-land-as-possible/1162-as-far-from-land-as-possible.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        int ans = -1;
+        queue<pair<int,int>> q;
+        int n = grid.size();
+        int m = grid[0].size();
+        bool landFlag = false;
+        bool waterFlag = false;
+        vector<vector<int>> visited(n,vector<int>(n,0));
+        for(int i = 0 ; i<grid.size() ; i++){
+            for(int j = 0 ; j<grid[0].size() ; j++){
+                if(grid[i][j]==1){
+                    q.push({i,j});
+                    visited[i][j] = 1;
+                    landFlag = true;
+                }
+                if(grid[i][j]==0){
+                    waterFlag = true;
+                }
+            }
+        }
+        if(!waterFlag||!landFlag){
+            return ans;
+        }
+
+        vector<int> d = {0,-1,0,1,0};
+        while(!q.empty()){
+            int count = q.size();
+            for(int i = 0 ; i<count ; i++){
+                pair<int,int> curr = q.front();
+                q.pop();
+                int r = curr.first;
+                int c = curr.second;
+                cout<<r<<" "<<c<<endl;
+                for(int i = 0 ; i<4 ; i++){
+                    int nr = r+d[i];
+                    int nc = c+d[i+1];
+                    if(nc>=0 && nr>=0 && nc<m&&nr<n && grid[nr][nc]==0&&visited[nr][nc]==0){
+                        q.push({nr,nc});
+                        visited[nr][nc]=1;
+                    }
+                }
+            }
+            ans++;
+
+        }
+        return ans;
+        
+    }
+};

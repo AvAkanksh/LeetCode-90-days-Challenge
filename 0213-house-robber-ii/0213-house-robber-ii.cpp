dd47e45d13c9d895201fd9e487dad5d94ff6b752
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2024-08-08 00:56:00 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (26.79%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,24 +1,26 @@
 class Solution {
 public:
-    int helper(vector<int>nums){
+    int helper(int start, vector<int>&nums){
         int n = nums.size();
-        int prev = 0;
-        int prev2 = 0;
-        for(int i = 0 ; i<n ; i++){
-            int curr = max(prev,prev2+nums[i]);
-            prev2 = prev;
-            prev = curr;
+        vector<int> dp(n,-1);
+        dp[0] = 0;
+        dp[1] = nums[start];
+        for(int i = 2; i<=n-1; i++){
+            // cout<<"i: "<<i<<"i-2 : "<<i-2<<"i+start :"<<i+start<<endl;
+            dp[i] = max(dp[i-2]+nums[i+start-1],dp[i-1]);
         }
-        return prev;
+        cout<<dp[n-1]<<endl;
+        return dp[n-1];
     }
+    
     int rob(vector<int>& nums) {
-        if(nums.size()==1){
+        int n = nums.size();
+        if(n==1){
             return nums[0];
         }
-        vector<int> temp1(nums.begin()+1,nums.end());
-        vector<int> temp2(nums.begin(),nums.end()-1);
-        int ans1 = helper(temp1);
-        int ans2 = helper(temp2);
+        int ans1 = helper(0,nums);
+        int ans2 = helper(1,nums);
+        
         return max(ans1,ans2);
     }
 };

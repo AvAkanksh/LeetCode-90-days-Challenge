6a59fcbc503d58c4480aa2195850e704706e7c78
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2024-08-08 23:03:39 +0530

    Time: 5 ms (36.49%), Space: 10.5 MB (31.68%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,42 +1,28 @@
-const auto fastio = [](){
-    ios_base::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-};
-
 class Solution {
 public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        fastio();
-        int n = obstacleGrid.size();
-        int m = obstacleGrid[0].size();
-        vector<int> prev(m,0);
-        for(int i = 0 ; i<n ; i++){
-            vector<int> temp(m,0);
-            for(int j = 0 ; j<m ; j++){
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        for(int i=0; i<m ; i++){
+            for(int j = 0 ; j<n ; j++){
+                // cout<<"i: "<<i<<"j: "<<j<<endl;
                 if(obstacleGrid[i][j]==1){
-                    temp[j] = 0;
-                }else{
-                    if(i==0 && j==0){
-                        temp[j] = 1;
+                    dp[i][j] = 0;
+                }
+                else{
+                    if(i==0 and j==0){
+                        dp[i][j] = 1;                    
                     }
-                    else{
-                        
-                        int up = 0;
-                        int left = 0;
-                        if(i>0){
-                            up = prev[j];
-                        }
-                        if(j>0){
-                            left = temp[j-1];
-                        }
-                        temp[j] = up + left;
+                    if(i>0){
+                        dp[i][j] += dp[i-1][j];
                     }
-
+                    if(j>0){
+                        dp[i][j] += dp[i][j-1];
+                    }
                 }
             }
-            prev = temp;
         }
-        return prev[m-1];
+        return dp[m-1][n-1];
     }
 };

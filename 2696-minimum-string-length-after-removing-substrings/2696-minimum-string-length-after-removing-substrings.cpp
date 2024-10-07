e0214129e966aee86af41e6595358c51893125e2
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2024-10-07 22:35:01 +0530

    Time: 10 ms (22.06%), Space: 10 MB (93.92%) - LeetHub

--- 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
+++ 2696-minimum-string-length-after-removing-substrings/2696-minimum-string-length-after-removing-substrings.cpp
@@ -2,15 +2,30 @@ class Solution {
 public:
     
     int minLength(string s) {
-        stack<char> st;
-        for(int i = 0 ; s[i]!='\0' ; i++){
-            if(!st.empty() && (st.top()=='A' || st.top()=='C') && st.top()+1==s[i]){
-                st.pop();
-            }else{
-                st.push(s[i]);
+        // Solution 1
+        // stack<char> st;
+        // for(int i = 0 ; s[i]!='\0' ; i++){
+        //     if(!st.empty() && (st.top()=='A' || st.top()=='C') && st.top()+1==s[i]){
+        //         st.pop();
+        //     }else{
+        //         st.push(s[i]);
+        //     }
+        // }
+        // return st.size();
+        
+        // Solution 2 : 2 Pointer Approach
+        int writer = 0;
+        int reader = 0;
+        while(reader<s.size()){
+            s[writer] = s[reader];
+            if(writer > 0 && (s[writer]=='B'|| s[reader]=='D') && s[writer]==s[writer-1]+1){
+                writer--;
             }
+            else{
+                writer++;
+            }
+            reader++;
         }
-    
-        return st.size();
+        return writer;
     }
 };

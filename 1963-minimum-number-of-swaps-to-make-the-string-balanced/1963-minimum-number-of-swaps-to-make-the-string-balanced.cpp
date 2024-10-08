d4efee50df12c4836d28c857cda6e5e8548ec4d0
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2024-10-08 14:26:13 +0530

    Time: 92 ms (92.62%), Space: 32.6 MB (79.33%) - LeetHub

--- 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 1963-minimum-number-of-swaps-to-make-the-string-balanced/1963-minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:    
     int minSwaps(string s) {
-        stack<char> st;
+        // Solution 1 : Stack Solution
+        // stack<char> st;
+        // for(char &ch : s){
+        //     if(ch=='['){
+        //         st.push(ch);
+        //     }
+        //     else if(!st.empty()){
+        //         st.pop();
+        //     }
+        // }
+        // return (st.size()+1)/2;
+
+        // Solution 2 : Optimal Solution
+        int count = 0;
         for(char &ch : s){
             if(ch=='['){
-                st.push(ch);
+                count++;
             }
-            else if(!st.empty()){
-                st.pop();
+            else if(count>0){
+                count--;
             }
         }
-        return (st.size()+1)/2;
+        return (count+1)/2;
+
     }
 };

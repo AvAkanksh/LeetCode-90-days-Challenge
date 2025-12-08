9bfdfd733b8e0121950fea51316eaa847c61370b
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2025-12-08 19:17:53 +0530

    Time: 315 ms (5.12%), Space: 121 MB (8.22%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+    bool check(int mid, vector<int>& bloomDay, int m, int k){
+        int ans = 0;
+        int count = 0;
+        for(auto x : bloomDay){
+            if(x<=mid){
+                count++;
+            }else{
+                ans+=int(count/k);
+                count=0;
+            }
+        }
+        cout<<"count"<<count<<endl;
+        cout<<"k"<<k<<endl;
+        ans += count/k;
+        cout<<"ans"<<ans<<endl;
+        // if(count>0){
+        //     ans+= int(count/k);
+        // }
+        return ans>=m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        set<int> s(bloomDay.begin(), bloomDay.end());
+        vector<int> v (s.begin(),s.end());
+        int n = v.size();
+        int l = 0;
+        int h = n-1;
+        int ans = INT_MAX;
+        while(l<=h){
+            int mid = ((h+l)/2);
+            int mid_val = v[mid];
+            cout<<mid<<endl;
+            if(mid<n and check(mid_val, bloomDay,m, k)){
+                ans = min(ans,mid_val);
+                h = mid-1;
+            }else{
+                l = mid+1;
+            }
+            // cout<<"low"<<l<<endl;
+            // cout<<"high"<<h<<endl;
+            // cout<<"mid"<<mid<<endl;
+        }
+        if(ans==INT_MAX){
+            return -1;
+        }
+        return ans;
+    }
+};

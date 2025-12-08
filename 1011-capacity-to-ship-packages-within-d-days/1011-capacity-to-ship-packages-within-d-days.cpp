cb5665967abc839516240017d2873fe281808ab6
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2025-12-08 20:00:35 +0530

    Time: 27 ms (5.45%), Space: 35.1 MB (17.56%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,36 +1,53 @@
 class Solution {
 public:
-    bool isPossible(int mid, vector<int>& weights, int &days){
-        int sum = 0 ;
-        int daysRequired = 1;
-        for(int i = 0 ; i<weights.size() ;i++){
-            sum+=weights[i];
-            if(sum>mid){
-                daysRequired++;
-                sum = weights[i];
-            }
-        }
-        return (daysRequired<=days);
-    }
-    
-    int shipWithinDays(vector<int>& weights, int days) {
-        int low=0;
-        int high=0;
-        for(auto weight : weights){
-            low = max(low,weight);
-            high += weight;
-        }
-        int mid;
-        int ans;
-        while(low<=high){
-            mid = low+(high - low)/2;
-            if(isPossible(mid,weights,days)){
-                ans = mid;
-                high = mid-1;
+    bool check(int mid, vector<int>& weights, int days){
+        int d = 0;
+        int w = 0;
+        for(int i = 0 ; i<weights.size() ; i++){
+            if(w+weights[i]<=mid){
+                w+=weights[i];
             }else{
-                low = mid+1;
+                d++;
+                w = weights[i];
             }
-            
+        }
+        if(w>0){
+            d++;
+        }
+        // int d = 0;
+        // int w = 0;
+        // for(auto x : weights){
+        //     w+=x;
+        //     if(w==mid){
+        //         d++;
+        //         w = 0;
+        //     }
+        //     if(w>mid){
+        //         d++;
+        //         w = x;
+        //     }
+        // }
+        // if(w>0){d++;}
+        cout<<d<<endl;
+        cout<<"--"<<endl;
+        return d<=days;
+    }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int l = weights[0];
+        for(auto x : weights){l = max(l,x);}
+        int h = accumulate(weights.begin(), weights.end(), 0);
+        // cout<<l<<endl;
+        // cout<<h<<endl;
+        int ans = h;
+        while(l<=h){
+            int mid = (h+l)/2;
+            cout<<"l: "<<l<<" h :"<<h<<" mid : "<<mid<<endl;
+            if(check(mid,weights,days)){
+                ans = min(ans,mid);
+                h = mid-1;
+            }else{
+                l = mid+1;
+            }
         }
         return ans;
     }

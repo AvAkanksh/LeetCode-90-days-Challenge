596b0f71d59852fbbd49b4b9e03204f185159ddd
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2025-12-08 23:07:48 +0530

    Time: 0 ms (100%), Space: 13.2 MB (59.05%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -1,35 +1,38 @@
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {
-        // int low = 0;
-        // int high = arr.size()-1;
-        // int idx = low;
-        // while(low<=high){
-        //     int mid = (low+high)/2;
-        //     if(arr[mid] - mid-1<k){
-        //         idx = mid;
-        //         low = mid+1;
-        //     }else if(arr[mid]-mid-1 >k){
-        //         high = mid-1;
-        //     }else{
-        //         break;
+        if(arr[0]>k){
+            return k;
+        }
+        if(arr[arr.size()-1]-arr.size()<k){
+            return k+arr.size();
+        }
+        int low = 0;
+        int high = arr.size()-1;
+        int idx = low;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(arr[mid] - mid-1<k){
+                low = mid+1;
+            }else{
+                high = mid-1;
+            }
+        }
+        return k + high +1 ;
+        // return arr[idx] + (k-(arr[idx]-idx-1));
+        // unordered_map<int,int> freq;
+        // for(auto x : arr){
+        //     freq[x]++;
+        // }
+        // int count = 0;
+        // for(int i = 1 ; i<=arr.size()+k ; i++){
+        //     if(freq.find(i)==freq.end()){
+        //         count++;
+        //     }
+        //     if(count==k){
+        //         return i;
         //     }
         // }
-        // cout<<idx<<endl;
-        // return arr[idx] + (k-(arr[idx]-idx-1));
-        unordered_map<int,int> freq;
-        for(auto x : arr){
-            freq[x]++;
-        }
-        int count = 0;
-        for(int i = 1 ; i<=arr.size()+k ; i++){
-            if(freq.find(i)==freq.end()){
-                count++;
-            }
-            if(count==k){
-                return i;
-            }
-        }
-        return -1;
+        // return -1;
     }
 };

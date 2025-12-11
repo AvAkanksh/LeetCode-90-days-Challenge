301ae78ae76b42c66a68ee42848ed36f9e61ec2c
Akanksh <49116510+AvAkanksh@users.noreply.github.com>
2025-12-11 13:20:37 +0530

    Time: 0 ms (100%), Space: 12.4 MB (75.61%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,45 +1,24 @@
-static auto fastio = [](){
-    std::ios_base::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-    std::cout.tie(nullptr);
-};
-
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        fastio();
         int n = nums.size();
         if(n==1){
-            return 0; 
+            return 0;
         }
         int low = 0;
         int high = n-1;
         while(low<=high){
-            int mid = low + (high-low)/2;
-            if(mid>0 and mid<n-1){
-                if(nums[mid]>nums[mid-1] and nums[mid]>nums[mid+1]){
-                    return mid;
-                }else if(nums[mid-1]>nums[mid]){
-                    high = mid-1;
-                }else {
-                    low = mid+1;
-                }
+            int mid = (low+high)/2;
+            // cout<<"n : "<<n<<" | mid : "<<mid<<endl;
+            if((mid>0 && mid<(n-1) && (nums[mid]>nums[mid-1]) && (nums[mid]> nums[mid+1]))|| (mid==0 && nums[mid]>nums[mid+1]) ||(mid==n-1)&& nums[mid]>nums[mid-1]){
+                return mid;
+            }else if(nums[mid+1]>nums[mid]){
+                low = mid+1;
             }else{
-                if(mid==0){
-                    if(nums[0]>nums[1]){
-                        return 0;
-                    }else{
-                        return 1;
-                    }
-                }else if(mid==n-1){
-                    if(nums[n-1]>nums[n-2]){
-                        return n-1;
-                    }else{
-                        return n-2;
-                    }
-                }
+                high= mid;
             }
         }
         return -1;
     }
+
 };
